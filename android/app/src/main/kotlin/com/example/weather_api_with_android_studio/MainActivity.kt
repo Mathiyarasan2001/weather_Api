c9f2ab76c50f2f0a9a13ba0eca19ac63747a4a7f
Your Name <you@example.com>
2024-06-26 11:29:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/weather_api_with_android_studio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_api_with_android_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String domain = "https://api.openweathermap.org/data/2.5/weather?";
+const String apiKey = "4bb3f314dd22e1779e6cf0a60daf991f";
+
